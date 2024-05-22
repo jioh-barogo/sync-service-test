1ea01802c11fd70036c8a7d1bc14f50b81aff1ed
ohjaein <jioh@barogo.com>
2024-05-22 09:35:01 +0900

    test'

--- server/src/main/java/serv/ServerApplication.java
+++ server/src/main/java/serv/ServerApplication.java
@@ -1,5 +1,6 @@
 package serv;
 
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
   public static void main(String[] args) {
-    System.out.println(1);
+
     SpringApplication.run(ServerApplication.class, args);
   }
 }
